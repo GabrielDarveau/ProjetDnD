d257e365f2dca5208bb0a08f454c28dd73ca4a34
GabrielDarveau <2132614@etu.cchic.ca>
2022-09-02 15:00:06 -0400

    Premier commit, création solution

---
+++ Projet DnD/Projet DnD/Perso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_DnD
+{
+    internal class Perso
+    {
+        //Attributs
+        string nom;
+        int niveau;
+        int pv;
+        int xp;
+        int[] habilites = new int[6];
+        Classe classePerso = new Classe();
+        Race racePerso = new Race();
+
+        //Constructeur
+        public Perso()
+        {
+
+        }
+        //Méthodes
+        public Classe GetClasse()
+        {
+            return classePerso;
+        }
+    }
+}
